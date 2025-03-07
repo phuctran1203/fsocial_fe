ad1e917c96c9c7b25d6ef996ed4477de458acdbf
Phuc Tran <phuctran12032004@gmail.com>
2025-03-07 19:12:48 +0700

    fixed admin policy page

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,12 +1,16 @@
 import AdminNav from "@/components/AdminNav";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "sonner";
 
 export default function AdminLayout() {
 	return (
-		<main className="p-3 flex gap-4 h-screen bg-background-lower">
-			<AdminNav />
-			<Outlet />
-		</main>
+		<>
+			<main className="p-3 flex gap-4 h-screen bg-background-lower">
+				<AdminNav />
+				<Outlet />
+			</main>
+			<Toaster position="top-center" />
+		</>
 	);
 }
