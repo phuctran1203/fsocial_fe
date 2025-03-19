5d376e552464049d542a036daeea8bac799ba242
phuongnamdpn98 <phuongnamdpn98@gmail.com>
2025-03-19 21:50:17 +0700

    fix admin profile api

--- src/api/adminProfileApi.js
+++ src/api/adminProfileApi.js
@@ -3,7 +3,7 @@ import API from './axiosInstance';
 export const getProfile = async (userId) => {
 
 	try {
-		const resp = await API.get(`/profile/profile-admin/${userId}/other`);
+		const resp = await API.get(`/profile/profile-admin/${userId}`);
 		const data = resp.data;
 		console.log("Resp profile: ", data);
 		return data;
