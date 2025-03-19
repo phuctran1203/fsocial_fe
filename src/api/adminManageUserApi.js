de2ca41412da7fcd99ccd562423e8ca871246a99
phuongnamdpn98 <phuongnamdpn98@gmail.com>
2025-03-19 17:37:15 +0700

    update api manage user

--- src/api/adminManageUserApi.js
+++ src/api/adminManageUserApi.js
@@ -12,9 +12,33 @@ export const getManageUser = async () => {
 	}
 };
 
+export const getBanUser = async () => {
+	try {
+		const resp = await API.get(`/banUser`);
+		const data = resp.data;
+		console.log("Resp ban manage User: ", data);
+		return data;
+	} catch (error) {
+		console.error("Error at ban manage User:", error);
+		return error.response?.data || null;
+	}
+};
+
+export const getUser = async () => {
+	try {
+		const resp = await API.get(`/getUser`);
+		const data = resp.data;
+		console.log("Resp manage User: ", data);
+		return data;
+	} catch (error) {
+		console.error("Error at manage User:", error);
+		return error.response?.data || null;
+	}
+};
+
 export const postManageUser = async (userID) => {
     try {
-        const resp = await API.post(`/manageUser/userID=${userID}`, dataObj);
+        const resp = await API.post(`/manageUser/userID=${userID}`);
         const data = resp.data;
         console.log("Resp manage user post: ", data);
         return data;
@@ -22,4 +46,16 @@ export const postManageUser = async (userID) => {
         console.error("Error at manage user post: ", error);
         return error.response?.data || null;
     }
+};
+
+export const searchManageUser = async (keyword) => {
+	try {
+		const resp = await API.get(`/manageUser/find?find_name=${keyword}`);
+		const data = resp.data;
+		console.log("Resp search manage user: ", data);
+		return data;
+	} catch (error) {
+		console.error("Error at search manage user:", error);
+		return error.response?.data || null;
+	}
 };
