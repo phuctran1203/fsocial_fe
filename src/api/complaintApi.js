349767bc18d713406ec3236d0c3759d1323f5729
CangNgo <cangngoo123@gmail.com>
2025-03-20 18:55:17 +0700

    merge admin

--- src/api/complaintApi.js
+++ src/api/complaintApi.js
@@ -18,3 +18,12 @@ export const getComplaint = async () =>{
         console.log("Error at get complaint", error);
     }
 }
+
+export const readingComplaint = async (id) => {
+    try {
+        const res = await API.put("/post/complaint/reading?complaint_id=" + id)
+        return res.data
+    } catch (error) {
+        console.log("Reading complaint err", error);
+    }
+}
