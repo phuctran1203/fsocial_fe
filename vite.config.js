2ab4e3d53c07e927fa7f1b3d280175e617951015
Phuc Tran <phuctran12032004@gmail.com>
2025-04-03 10:44:49 +0700

    add library auto mififile html

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import removeConsole from "vite-plugin-remove-console";
 import path from "node:path";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
 	plugins: [
 		react(),
 		removeConsole({
 			exclude: ["error", "warn"], // Giữ lại console.error và console.warn
+		}),
+		createHtmlPlugin({
+			minify: true, // Tự động minify HTML
+			transformHtml: (html) =>
+				html
+					.replace(/<!--[\s\S]*?-->/g, "") // Xóa comment HTML <!-- -->
+					.replace(/\/\/[^\n\r]*/g, ""), // Xóa comment kiểu // trong script
 		}),
 	],
 	// cần đoạn này để cài shadcn
